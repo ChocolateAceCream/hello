84d39d12d10c9955f0d4d4f892a06eba65b663f3
ChocolateAceCream <nuodi@hotmail.com>
2015-07-06 19:00:37 -0400

    fixed

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,17 @@
 Rails.application.routes.draw do
   get 'welcome/index'
-
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   resources :articles
   # You can have the root of your site routed with "root"
+  get 'articles/:id/display', to: 'articles#display', as: 'display_article'
   root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
-
+     
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
