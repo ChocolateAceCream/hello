9e0bf22ef25c7bf8842eb2059931002dec75b43e
ChocolateAceCream <nuodi@hotmail.com>
2015-07-08 17:29:10 -0400

    user login

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,7 @@
 class ArticlesController < ApplicationController
+
+    http_basic_authenticate_with name: "di", password: "di880817", except: [:index, :show]
+
     def index
              @articles = Article.all
     end
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,17 @@
 class CommentsController < ApplicationController
+        http_basic_authenticate_with name: "di", password: "di880817", only: :destroy
         def create
             @article = Article.find(params[:article_id])
             @comment = @article.comments.create(comment_params)
             redirect_to display_article_path(@article)
-          end
+        end
+
+        def destroy
+             @article = Article.find(params[:article_id])
+             @comment = @article.comments.find(params[:id])
+             @comment.destroy
+             redirect_to display_article_path(@article)
+        end
 
       private
         def comment_params
